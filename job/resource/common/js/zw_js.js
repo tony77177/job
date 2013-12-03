b8ae9db914e4078d1ac0e2b031da04421f4936a0
tony77177 <tony77177@gmail.com>
2013-12-03 01:06:07 -0800

    样式修改

--- job/resource/common/js/zw_js.js
+++ job/resource/common/js/zw_js.js
@@ -5,7 +5,7 @@ $(document).ready(function(e) {
 	//偶数行背景变色
 	$('.zw_list li:odd').css("background","#e5f2cf");
 	//提示框弹出代码
-	$('.zw_list_a').tooltip({
+	$('.zw_tanchu').tooltip({
 		placement:'auto top'
 		});
 
