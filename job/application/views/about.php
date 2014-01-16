abb10d30035a459a730c0018ec82d99fcbb14def
tony77177 <tony77177@gmail.com>
2014-01-16 01:45:34 -0800

    增加meta关键词

--- job/application/views/about.php
+++ job/application/views/about.php
@@ -10,7 +10,9 @@
 
 <?php require_once('common/navbar.php'); ?>
 
-    <div class="container" style="margin: 50px auto;">
+    <div class="container" style="margin: 50px auto;font-size: 16px;">
+        <p style="text-indent: 30px;">贵州找工作搜索了贵州大部分的招聘信息，<strong>让您告别广告的烦恼</strong>，节约您的时间，<strong>用最少的时间阅读最全面的招聘信息</strong></p>
+
         <p style="text-indent: 30px;">我们提供给大家一个聚合阅读的场所，将贵州大部分招聘信息聚合在此，让您用最少的时间看到最多信息！</p>
 
         <p style="text-indent: 30px;">但是，我们还很年轻，需要您的支持，找工作，上贵州找工作吧！</p>
@@ -19,6 +21,7 @@
                                              href="http://shang.qq.com/wpa/qunwpa?idkey=f40c5e3834b11b56a8a13cf162b09c03e24f7b656eba611f7e17bc14ba1a1b2b"><img
                     border="0" src="http://pub.idqqimg.com/wpa/images/group.png" alt="贵州找工作"
                     title="加入贵州找工作"></a></p>
+        <p style="text-indent: 30px;margin-left: 100px;margin-top: 30px;" class="text-danger">注：所有信息均来源于网络，我们不能保证信息的可靠性。每条信息来源均注明了出处，<br>如果您觉得有侵犯版权的问题，请及时和我们联系，我们将尽快删除相关数据信息！</p>
     </div>
 
 <?php require_once('common/footer.php'); ?>
--- job/application/views/common/header.php
+++ job/application/views/common/header.php
@@ -7,7 +7,10 @@
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="贵州找工作 贵州找工作网 贵州人才 贵阳人才 贵阳求职 贵州招聘 银行招聘 贵阳银行 贵州公务员 贵州省考 贵州公务员考试网 贵州人事考试 贵州贵阳人才租赁 贵州贵阳人才招聘会">
+    <meta name="description" content="贵州找工作搜索了贵州大部分的招聘信息，让您告别广告的烦恼，节约您的时间，用最少的时间阅读最全面的招聘信息。请收藏我们的网址：http://job.freedomdream.cn">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="baidu-site-verification" content="5bTRK9ebRb" />
 
     <!-- Loading ICON -->
     <link href="resource/common/img/favicon.ico" rel="shortcut icon">
